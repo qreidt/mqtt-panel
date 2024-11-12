b934c25c5f3c991b63af0ea9c57e5ed639b65c6b
Caio Reidt <caioreidt@gmail.com>
2024-11-12 00:49:41 -0300

    MQTT Client Authentication API Route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 if (app()->isLocal() && class_exists(Controllers\DevController::class)) {
     Route::get('/dev', Controllers\DevController::class);
 }
+
+Route::post('/mqtt/auth', Controllers\Api\AuthApiTokenController::class);
